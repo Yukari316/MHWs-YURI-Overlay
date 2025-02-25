911df77b057d4a8f272ae30c93e72605345f5288
GreenComfyTea <klunics97@gmail.com>
2025-02-25 21:06:27 +0200

    chore: Add LogManager.Debug

--- src/Infrastructure/LogManager/LogManager.cs
+++ src/Infrastructure/LogManager/LogManager.cs
@@ -4,6 +4,7 @@ namespace YURI_Overlay;
 
 internal static class LogManager
 {
+
 	public static void Info(object value)
 	{
 		API.LogInfo($"[{DateTime.Now.ToLongTimeString()}] [{Constants.ModName}] {value}");
@@ -17,5 +18,10 @@ internal static class LogManager
 	public static void Error(object value)
 	{
 		API.LogError($"[{DateTime.Now.ToLongTimeString()}] [{Constants.ModName}] {value}");
+	}
+
+	public static void Debug(object value)
+	{
+		API.LogInfo($"[{DateTime.Now.ToLongTimeString()}] [{Constants.ModName}] {value}");
 	}
 }
