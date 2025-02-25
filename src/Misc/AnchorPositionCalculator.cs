f116f028c4918f3e324c7b8ae8716daf03b5df2a
GreenComfyTea <klunics97@gmail.com>
2025-02-25 21:06:14 +0200

    fix: Position anchor not working

--- src/Misc/AnchorPositionCalculator.cs
+++ src/Misc/AnchorPositionCalculator.cs
@@ -8,41 +8,41 @@ internal static class AnchorPositionCalculator
 {
 	public static Vector2 Convert(AnchoredPositionCustomization anchoredPositionCustomization)
 	{
-		var screenRegion = ImGui.GetWindowSize();
+		var displaySize = ImGui.GetIO().DisplaySize;
 
 		return anchoredPositionCustomization.AnchorEnum switch
 		{
 			Anchors.TopCenter => new Vector2(
-								(screenRegion.X / 2f) + anchoredPositionCustomization.x,
+								(displaySize.X / 2f) + anchoredPositionCustomization.x,
 								anchoredPositionCustomization.y
 							),
 			Anchors.TopRight => new Vector2(
-								screenRegion.X + anchoredPositionCustomization.x,
+								displaySize.X + anchoredPositionCustomization.x,
 								anchoredPositionCustomization.y
 							),
 			Anchors.CenterLeft => new Vector2(
 								anchoredPositionCustomization.x,
-								(screenRegion.Y / 2f) + anchoredPositionCustomization.y
+								(displaySize.Y / 2f) + anchoredPositionCustomization.y
 							),
 			Anchors.Center => new Vector2(
-								(screenRegion.X / 2f) + anchoredPositionCustomization.x,
-								(screenRegion.Y / 2f) + anchoredPositionCustomization.y
+								(displaySize.X / 2f) + anchoredPositionCustomization.x,
+								(displaySize.Y / 2f) + anchoredPositionCustomization.y
 							),
 			Anchors.CenterRight => new Vector2(
-								screenRegion.X + anchoredPositionCustomization.x,
-								(screenRegion.Y / 2f) + anchoredPositionCustomization.y
+								displaySize.X + anchoredPositionCustomization.x,
+								(displaySize.Y / 2f) + anchoredPositionCustomization.y
 							),
 			Anchors.BottomLeft => new Vector2(
 								anchoredPositionCustomization.x,
-								screenRegion.Y + anchoredPositionCustomization.y
+								displaySize.Y + anchoredPositionCustomization.y
 							),
 			Anchors.BottomCenter => new Vector2(
-								(screenRegion.X / 2f) + anchoredPositionCustomization.x,
-								screenRegion.Y + anchoredPositionCustomization.y
+								(displaySize.X / 2f) + anchoredPositionCustomization.x,
+								displaySize.Y + anchoredPositionCustomization.y
 							),
 			Anchors.BottomRight => new Vector2(
-								screenRegion.X + anchoredPositionCustomization.x,
-								screenRegion.Y + anchoredPositionCustomization.y
+								displaySize.X + anchoredPositionCustomization.x,
+								displaySize.Y + anchoredPositionCustomization.y
 							),
 			_ => new Vector2(
 								anchoredPositionCustomization.x,
