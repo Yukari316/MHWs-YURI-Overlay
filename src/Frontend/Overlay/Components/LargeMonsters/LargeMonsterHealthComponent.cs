b58779b27587dfe54bd8c9529764ef9be7b36bf4
GreenComfyTea <klunics97@gmail.com>
2025-02-27 19:48:48 +0200

    fix: Fix health component offset not working

--- src/Frontend/Overlay/Components/LargeMonsters/LargeMonsterHealthComponent.cs
+++ src/Frontend/Overlay/Components/LargeMonsters/LargeMonsterHealthComponent.cs
@@ -12,9 +12,13 @@ internal class LargeMonsterHealthComponent
 	private readonly LabelElement _healthPercentageLabelElement;
 	private readonly BarElement _healthBarElement;
 
+	private readonly Func<LargeMonsterHealthComponentCustomization> customizationAccessor;
+
 	public LargeMonsterHealthComponent(LargeMonster largeMonster, Func<LargeMonsterHealthComponentCustomization> customizationAccessor)
 	{
 		_largeMonster = largeMonster;
+
+		this.customizationAccessor = customizationAccessor;
 
 		_healthValueLabelElement = new LabelElement(() => customizationAccessor().ValueLabel);
 		_healthPercentageLabelElement = new LabelElement(() => customizationAccessor().PercentageLabel);
@@ -23,9 +27,11 @@ internal class LargeMonsterHealthComponent
 
 	public void Draw(ImDrawListPtr backgroundDrawList, Vector2 position, float opacityScale = 1f)
 	{
-		_healthBarElement.Draw(backgroundDrawList, position, _largeMonster.HealthPercentage, opacityScale);
-		_healthPercentageLabelElement.Draw(backgroundDrawList, position, opacityScale, _largeMonster.HealthPercentage);
-		_healthValueLabelElement.Draw(backgroundDrawList, position, opacityScale, _largeMonster.Health,
-			_largeMonster.MaxHealth);
+		var offset = this.customizationAccessor().Offset;
+		var offsetPosition = new Vector2(position.X + offset.X, position.Y + offset.Y);
+
+		_healthBarElement.Draw(backgroundDrawList, offsetPosition, _largeMonster.HealthPercentage, opacityScale);
+		_healthPercentageLabelElement.Draw(backgroundDrawList, offsetPosition, opacityScale, _largeMonster.HealthPercentage);
+		_healthValueLabelElement.Draw(backgroundDrawList, offsetPosition, opacityScale, _largeMonster.Health, _largeMonster.MaxHealth);
 	}
 }
--- src/Infrastructure/ConfigManager/Default/DefaultConfig.cs
+++ src/Infrastructure/ConfigManager/Default/DefaultConfig.cs
@@ -8,76 +8,81 @@ internal static class DefaultConfig
 
 		config.localization = Constants.DefaultLocalization;
 
-		config.LargeMonsterUI.Dynamic.Enabled = true;
-		config.LargeMonsterUI.Dynamic.Settings.HideDeadOrCaptured = true;
-		config.LargeMonsterUI.Dynamic.Settings.RenderHighlightedMonster = true;
-		config.LargeMonsterUI.Dynamic.Settings.RenderNotHighlightedMonsters = true;
-		config.LargeMonsterUI.Dynamic.Settings.OpacityFalloff = true;
-		config.LargeMonsterUI.Dynamic.Settings.MaxDistance = 300f;
+		//config.LargeMonsterUI.Dynamic.Enabled = true;
+		//config.LargeMonsterUI.Dynamic.Settings.HideDeadOrCaptured = true;
+		//config.LargeMonsterUI.Dynamic.Settings.RenderHighlightedMonster = true;
+		//config.LargeMonsterUI.Dynamic.Settings.RenderNotHighlightedMonsters = true;
+		//config.LargeMonsterUI.Dynamic.Settings.OpacityFalloff = true;
+		//config.LargeMonsterUI.Dynamic.Settings.MaxDistance = 300f;
 
-		config.LargeMonsterUI.Dynamic.WorldOffset.X = 0f;
-		config.LargeMonsterUI.Dynamic.WorldOffset.Y = 500f;
-		config.LargeMonsterUI.Dynamic.WorldOffset.Z = 0f;
+		//config.LargeMonsterUI.Dynamic.WorldOffset.X = 0f;
+		//config.LargeMonsterUI.Dynamic.WorldOffset.Y = 500f;
+		//config.LargeMonsterUI.Dynamic.WorldOffset.Z = 0f;
 
-		config.LargeMonsterUI.Dynamic.Offset.X = 0f;
-		config.LargeMonsterUI.Dynamic.Offset.Y = 0f;
+		//config.LargeMonsterUI.Dynamic.Offset.X = 0f;
+		//config.LargeMonsterUI.Dynamic.Offset.Y = 0f;
 
-		config.LargeMonsterUI.Dynamic.NameLabel.Visible = true;
-		config.LargeMonsterUI.Dynamic.NameLabel.Format = "{0}";
-		config.LargeMonsterUI.Dynamic.NameLabel.Settings.RightAlignmentShift = 0;
-		config.LargeMonsterUI.Dynamic.NameLabel.Offset.X = -96f + 3f;
-		config.LargeMonsterUI.Dynamic.NameLabel.Offset.Y = 0f;
-		config.LargeMonsterUI.Dynamic.NameLabel.Color.colorInfo.Rgba = 0xFFFFFFFF;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Visible = true;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Format = "{0}";
+		//config.LargeMonsterUI.Dynamic.NameLabel.Settings.RightAlignmentShift = 0;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Offset.X = -96f + 3f;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Offset.Y = 0f;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Color.colorInfo.Rgba = 0xFFFFFFFF;
 
-		config.LargeMonsterUI.Dynamic.NameLabel.Shadow.Visible = true;
-		config.LargeMonsterUI.Dynamic.NameLabel.Shadow.Offset.X = 2f;
-		config.LargeMonsterUI.Dynamic.NameLabel.Shadow.Offset.Y = 2f;
-		config.LargeMonsterUI.Dynamic.NameLabel.Shadow.Color.colorInfo.Rgba = 0x000000FF;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Shadow.Visible = true;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Shadow.Offset.X = 2f;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Shadow.Offset.Y = 2f;
+		//config.LargeMonsterUI.Dynamic.NameLabel.Shadow.Color.colorInfo.Rgba = 0x000000FF;
 
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Visible = true;
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Format = "{0:F0}/{1:F0}";
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Settings.RightAlignmentShift = 13;
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Offset.X = 4f;
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Offset.Y = 16f;
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Color.colorInfo.Rgba = 0xFFFFFFFF;
+		//config.LargeMonsterUI.Dynamic.Health.Visible = true;
 
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Shadow.Visible = true;
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Shadow.Offset.X = 2f;
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Shadow.Offset.Y = 2f;
-		config.LargeMonsterUI.Dynamic.Health.ValueLabel.Shadow.Color.colorInfo.Rgba = 0x000000FF;
+		//config.LargeMonsterUI.Dynamic.Health.Offset.X = 0f;
+		//config.LargeMonsterUI.Dynamic.Health.Offset.Y = 15f;
 
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Visible = false;
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Format = "{0:P1}";
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Settings.RightAlignmentShift = 6;
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Offset.X = 49f;
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Offset.Y = 28f;
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Color.colorInfo.Rgba = 0xFFFFFFFF;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Visible = true;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Format = "{0:F0}/{1:F0}";
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Settings.RightAlignmentShift = 13;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Offset.X = 4f;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Offset.Y = 16f;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Color.colorInfo.Rgba = 0xFFFFFFFF;
 
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Shadow.Visible = true;
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Shadow.Offset.X = 2f;
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Shadow.Offset.Y = 2f;
-		config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Shadow.Color.colorInfo.Rgba = 0x000000FF;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Shadow.Visible = true;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Shadow.Offset.X = 2f;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Shadow.Offset.Y = 2f;
+		//config.LargeMonsterUI.Dynamic.Health.ValueLabel.Shadow.Color.colorInfo.Rgba = 0x000000FF;
 
-		config.LargeMonsterUI.Dynamic.Health.Bar.Visible = true;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Settings.FillDirection = FillDirections.LeftToRight;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Offset.X = -96f;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Offset.Y = 16f;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Size.Width = 192f;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Size.Height = 7f;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Visible = false;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Format = "{0:P1}";
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Settings.RightAlignmentShift = 6;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Offset.X = 49f;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Offset.Y = 28f;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Color.colorInfo.Rgba = 0xFFFFFFFF;
 
-		config.LargeMonsterUI.Dynamic.Health.Bar.Colors.Background.StartInfo.Rgba = 0xFFFFFF59;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Colors.Background.EndInfo.Rgba = 0xFFFFFF59;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Colors.Foreground.StartInfo.Rgba = 0x164D35FF;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Colors.Foreground.EndInfo.Rgba = 0x75A55EFF;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Shadow.Visible = true;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Shadow.Offset.X = 2f;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Shadow.Offset.Y = 2f;
+		//config.LargeMonsterUI.Dynamic.Health.PercentageLabel.Shadow.Color.colorInfo.Rgba = 0x000000FF;
 
-		config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Visible = true;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Thickness = 3f;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Offset = 0f;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Style = OutlineStyles.Inside;
-		config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Color.colorInfo.Rgba = 0x00000059;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Visible = true;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Settings.FillDirection = FillDirections.LeftToRight;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Offset.X = -96f;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Offset.Y = 16f;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Size.Width = 192f;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Size.Height = 7f;
 
-		config.LargeMonsterUI.Static.Enabled = true;
-		config.LargeMonsterUI.Static.Settings.HideDeadOrCaptured = true;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Colors.Background.StartInfo.Rgba = 0xFFFFFF59;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Colors.Background.EndInfo.Rgba = 0xFFFFFF59;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Colors.Foreground.StartInfo.Rgba = 0x164D35FF;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Colors.Foreground.EndInfo.Rgba = 0x75A55EFF;
+
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Visible = true;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Thickness = 3f;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Offset = 0f;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Style = OutlineStyles.Inside;
+		//config.LargeMonsterUI.Dynamic.Health.Bar.Outline.Color.colorInfo.Rgba = 0x00000059;
+
+		//config.LargeMonsterUI.Static.Enabled = true;
+		//config.LargeMonsterUI.Static.Settings.HideDeadOrCaptured = true;
 		//config.LargeMonsterUI.@Static.settings.renderHighlightedMonster = true;
 		//config.LargeMonsterUI.@Static.settings.renderNotHighlightedMonsters = true;
 
@@ -104,14 +109,15 @@ internal static class DefaultConfig
 		config.LargeMonsterUI.Static.NameLabel.Shadow.Color.colorInfo.Rgba = 0x000000CC;
 
 		config.LargeMonsterUI.Static.Health.Visible = true;
+
 		config.LargeMonsterUI.Static.Health.Offset.X = 0f;
-		config.LargeMonsterUI.Static.Health.Offset.Y = 0f;
+		config.LargeMonsterUI.Static.Health.Offset.Y = 15f;
 
 		config.LargeMonsterUI.Static.Health.ValueLabel.Visible = true;
 		config.LargeMonsterUI.Static.Health.ValueLabel.Format = "{0:F1}/{1:F0}";
 		config.LargeMonsterUI.Static.Health.ValueLabel.Settings.RightAlignmentShift = 0;
 		config.LargeMonsterUI.Static.Health.ValueLabel.Offset.X = 7f;
-		config.LargeMonsterUI.Static.Health.ValueLabel.Offset.Y = 24f;
+		config.LargeMonsterUI.Static.Health.ValueLabel.Offset.Y = 9f;
 		config.LargeMonsterUI.Static.Health.ValueLabel.Color.colorInfo.Rgba = 0xFFFFFFCC;
 
 		config.LargeMonsterUI.Static.Health.ValueLabel.Shadow.Visible = true;
@@ -123,7 +129,7 @@ internal static class DefaultConfig
 		config.LargeMonsterUI.Static.Health.PercentageLabel.Format = "{0:P1}";
 		config.LargeMonsterUI.Static.Health.PercentageLabel.Settings.RightAlignmentShift = 6;
 		config.LargeMonsterUI.Static.Health.PercentageLabel.Offset.X = 245f;
-		config.LargeMonsterUI.Static.Health.PercentageLabel.Offset.Y = 24f;
+		config.LargeMonsterUI.Static.Health.PercentageLabel.Offset.Y = 9f;
 		config.LargeMonsterUI.Static.Health.PercentageLabel.Color.colorInfo.Rgba = 0xFFFFFFCC;
 
 		config.LargeMonsterUI.Static.Health.PercentageLabel.Shadow.Visible = true;
@@ -134,7 +140,7 @@ internal static class DefaultConfig
 		config.LargeMonsterUI.Static.Health.Bar.Visible = true;
 		config.LargeMonsterUI.Static.Health.Bar.Settings.FillDirection = FillDirections.LeftToRight;
 		config.LargeMonsterUI.Static.Health.Bar.Offset.X = 0f;
-		config.LargeMonsterUI.Static.Health.Bar.Offset.Y = 15f;
+		config.LargeMonsterUI.Static.Health.Bar.Offset.Y = 0f;
 		config.LargeMonsterUI.Static.Health.Bar.Size.Width = 297f;
 		config.LargeMonsterUI.Static.Health.Bar.Size.Height = 12f;
 
